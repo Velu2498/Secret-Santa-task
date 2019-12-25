d46b85c9c79ab4ff732b32f99eba7da9d12262b3
vel murugan <velu020498@gmail.com>
2019-12-25 10:30:44 +0530

    add node file

--- node server/index.js
+++ node server/index.js
@@ -1,69 +1,220 @@
-const express =require("express")
-const app=express()
-const cors=require("cors")
-const bodyp=require("body-parser")
-const mongodb=require("mongodb")
-const mongoclient=mongodb.MongoClient
-const url="mongodb://localhost:27017"
+const express = require("express")
+const app = express()
+const cors = require("cors")
+const bodyp = require("body-parser")
+const mongodb = require("mongodb")
+const mongoclient = mongodb.MongoClient
+const url = "mongodb://localhost:27017"
 const port = 3000
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+var jwt = require('jsonwebtoken');
+var token = jwt.sign({ foo: 'bar' }, 'shhhhh');
 
 app.use(cors());
 app.use(bodyp.json());
 
-//getting data from form & post to data base
-app.post('/data',function(req,res){
-    
-    mongoclient.connect(url, function(err, client) {
-      if (err) throw err;
-      
-      // console.log(req.body)
-      
-      db=client.db("myflim")
+//authentication
+function authentication(req, res, nxt) {
+  // var incomingToken=req.header("Authorization");
+  var incomingToken=req.body.token
+console.log(incomingToken)
+  jwt.verify(incomingToken,"code",function(err,decode){
+    console.log(decode)
+    if(decode !== undefined){
+      nxt()
+    }
+    else{
+      res.status(401).json({
+        mssg:"not permitted"
+      })
+    }
+  })
+}
 
-      db.collection("authors").insertOne(req.body,(err,data)=>{
+
+
+//getting data from form & post to data base
+// app.post('/data',function(req,res){
+
+//     mongoclient.connect(url, function(err, client) {
+//       if (err) throw err;
+
+//       // console.log(req.body)
+
+//       db=client.db("myflim")
+
+//       db.collection("authors").insertOne(req.body,(err,data)=>{
+//         if (err) throw err;
+
+//         // console.log(data)
+
+//         res.json({"mess":"inserted data in data base"})
+
+//       }
+//     )
+//         client.close()
+
+//     });
+// })
+
+
+//getting data from form & post to data base encrypted 
+app.post('/data', function (req, res) {
+
+  mongoclient.connect(url, function (err, client) {
+
+    if (err) throw err;
+
+    db = client.db("myflim")
+
+    bcrypt.genSalt(saltRounds, function (err, salt) {
+
+      if (err) throw err;
+
+      bcrypt.hash(req.body.password, salt, function (err, hash) {
+
+        req.body.password = hash
+
+        console.log(req.body)
+
         if (err) throw err;
 
-        // console.log(data)
+        db.collection("authors").insertOne(req.body, function (err, data) {
 
-        res.json({"mess":"inserted data in data base"})
-    
-      }
-    )
+          if (err) throw err;
+          // console.log(err)
+          res.status(200).json({ "mess": "inserted data in data base" })
+
+
+          // console.log(data)
+        })
         client.close()
-    
-    });
+
+      })
+
+    })
+  })
+
 })
 
 
-//appending data to table from data base
-app.get('/data',function(req,res){
-  
+//login validation
+// app.post('/login',function(req,res){
+
+//     mongoclient.connect(url, function(err, client) {
+//       if (err) throw err;
+
+//       db=client.db("myflim")
+      
+//       // console.log(req.body)
+//       var result=db.collection("authors").findOne({email:req.body.email})
+
+//       result.then(
+//         function (userdata){
+//           console.log(userdata)
+//           // console.log(req.body)
+
+//           if(userdata == null){
+//             res.json({
+//               mess:"mail id not found"
+//             })
+//           }
+
+//           else{
+//           bcrypt.compare(req.body.password,userdata.password,function (err,hashresult){
+//             if(hashresult==true){
+//               res.json({
+//                 mess:"welcome"
+//               })
+//             }
+//             else{
+//               res.json({
+//                 mess:"invalid password"
+//               })
+//             }
+//           })
+//           }
+
+//           client.close()
+//         })
+
+//       })
+
+//     })
+
+
+//login with token
+app.post('/login',function(req,res){
+
   mongoclient.connect(url, function(err, client) {
     if (err) throw err;
+
+    db=client.db("myflim")
     
     // console.log(req.body)
-    
-    db=client.db("myflim")
+    var result=db.collection("authors").findOne({email:req.body.email})
 
-    var userCurso=db.collection("authors").find().toArray();
-    
-    userCurso.then((daa)=>{
-      res.json(daa)
+    result.then(
+      function (userdata){
+        console.log(userdata)
+        // console.log(req.body)
+
+        if(userdata == null){
+          res.json({
+            mess:"mail id not found"
+          })
+        }
+
+        else{
+        bcrypt.compare(req.body.password,userdata.password,function (err,hashresult){
+          if(hashresult==true){
+
+            jwt.sign({
+              exp:Math.floor(Date.now()/1000) + (60 *60),
+              data:'foobar'
+            },"code",function(err,token){
+              if (err) throw err;
+              console.log(token)
+              res.json({
+                mess:"welcome",
+                token: token
+              })
+            })
+
+          }
+
+          else{
+            res.json({
+              mess:"invalid password"
+            })
+          }
+        })
+        }
+
+        client.close()
+      })
+
     })
-    
-    client.close()
-    
-  });
-})
+
+  })
+
 
 
 //message on empty url
-app.get('/',function(req,res){
-    res.send("<h1>hellow switch to data</h1>")
+app.get('/',function (req, res) {
+  res.send("<h1>hellow switch to data</h1>")
+})
+
+//check token
+app.get('/afterlogin', authentication , function (req, res) {
+  res.json({
+    "key":"val"
+  })
 })
 
 //port listering
-app.listen(port, () => {console.log(`Example app listening on port ${port}!`) })
+app.listen(port, () => { console.log(`Example app listening on the port ${port}!`) })
 
 
 
